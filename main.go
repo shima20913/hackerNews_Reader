16d7a50965b0b74a1a0a4c73050620ee93ab8784
TakashimaY <yousukes53@gmail.com>
2024-08-05 09:40:49 +0900

    get hackerNews top stories

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/joho/godotenv"
 )
@@ -16,4 +17,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("error load .env file")
 	}
+
+	hackerNewsAPI := "https://hacker-news.firebaseio.com/v0/topstories.json"
+	response, err := http.Get(hackerNewsAPI)
+	if err != nil {
+		log.Fatalf("failed to get hackerNews stories:%v", err)
+	}
+	defer response.Body.Close()
+
 }
